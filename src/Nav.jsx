77a40e9dad7b93a0faf784adbf74841dede2c92a
Godwill993 <lendzelegoodwill@gmail.com>
2025-09-06 21:18:08 +0100

    my first commit

---
+++ src/Nav.jsx
@@ -0,0 +1,42 @@
+
+import './Nav.css'
+import React, { useEffect, useState } from 'react';
+
+function Nav(){
+const App = () => {
+  const [theme, setTheme] = useState('light'); // default is light
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.body.className = ''; // clear any existing classes
+    document.body.classList.add(`${theme}-mode`);
+  }, [theme]);
+
+
+
+
+    return(
+        <nav>
+            <img className='logo' src="/src/assets/CHAK-LOGO.png" alt="" />
+            <div className="nav-links">
+                <li>HOME</li>
+                <li>SERVICES</li>
+                <li>CARGO</li>
+                <li>ARGENCIES</li>
+            </div>
+            <div className="HAM">
+                <span className='hamm'></span>
+                <span className='hamm'></span>
+                <span className='hamm'></span>
+
+            </div>
+            <button onClick={toggleTheme}>CHANGE THEME</button>
+        </nav>
+    )
+} }
+
+export default Nav;
